39079c9e547df3f47d05508f1a433db15ddb7519
Anant Tandon <36410163+AnantTan@users.noreply.github.com>
2021-05-05 13:27:06 +0530

    Data
    More data and model retrained

--- main.py
+++ main.py
@@ -86,9 +86,9 @@ print("Training data created")
 # 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
-model.add(Dense(128, input_shape=(len(train_x[0]),), activation='relu'))
+model.add(Dense(256, input_shape=(len(train_x[0]),), activation='relu'))
 model.add(Dropout(0.5))
-model.add(Dense(64, activation='relu'))
+model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
@@ -97,7 +97,7 @@ sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
 
 # fitting and saving the model 
-hist = model.fit(np.array(train_x), np.array(train_y), epochs=400, batch_size=5, verbose=1)
+hist = model.fit(np.array(train_x), np.array(train_y), epochs=500, batch_size=5, verbose=1)
 model.save('chatbot_model.h5', hist)
 
 print("model created")
