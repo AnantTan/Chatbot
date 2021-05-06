4abd43fe7ab348d17dcbf2923f937dc5ccb4c7e3
Anant Tandon <36410163+AnantTan@users.noreply.github.com>
2021-05-06 15:40:44 +0530

    Comments
    Some more comments added for clarifiication.

--- main.py
+++ main.py
@@ -82,12 +82,12 @@ print("Training data created")
 
 
 # Create model - 3 layers. 
-# First layer 128 neurons, second layer 64 neurons and 
+# First layer 256 neurons, second layer 128 neurons and 
 # 3rd output layer contains number of neurons
 # equal to number of intents to predict output intent with softmax
 model = Sequential()
 model.add(Dense(256, input_shape=(len(train_x[0]),), activation='relu'))
-model.add(Dropout(0.5))
+model.add(Dropout(0.5))# ignoring the randomly selected 
 model.add(Dense(128, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
